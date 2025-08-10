5fbc11453671e5d8b1b597fc9f35c8de66edaee8
Allan Adriano <allanadriano.n@gmail.com>
2025-08-10 14:48:22 -0300

    Section about em progresso

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,20 +8,41 @@ const menuActive = () => {
   openMenuIcon.classList.toggle("hide");
   closeMenuIcon.classList.toggle("show");
 };
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('header');
+
+/*navigation bar effect for larger screens*/
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
   const isDesktop = window.innerWidth >= 992;
 
   if (isDesktop && window.scrollY > 50) {
-      header.classList.add('scrolled');
+    header.classList.add("scrolled");
   } else {
-      header.classList.remove('scrolled');
+    header.classList.remove("scrolled");
   }
 });
 
-window.addEventListener('resize', function() {
-  const header = document.querySelector('header');
+window.addEventListener("resize", function () {
+  const header = document.querySelector("header");
   if (window.innerWidth < 992) {
-      header.classList.remove('scrolled');
+    header.classList.remove("scrolled");
   }
+});
+
+document.querySelectorAll(".mobile-menu a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // Fecha o menu
+    menuMobile.classList.remove("active");
+    openMenuIcon.classList.remove("hide");
+    closeMenuIcon.classList.remove("show");
+
+    // Rola até a seção clicada (caso queira animação suave)
+    const targetId = link.getAttribute("href");
+    if (targetId.startsWith("#")) {
+      e.preventDefault();
+      document.querySelector(targetId).scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
 });
